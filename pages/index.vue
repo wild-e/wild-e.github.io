a6430ae620d8159fc1242768115c8487d04a9210
wild-e <wild.emmanuelle@gmail.com>
2022-08-22 21:11:25 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1
+    class="text-4xl font-bold"
+  >
+    Hello world !
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
