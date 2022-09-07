1b0533a9a032ca21917607daeff848a7071c8860
wild-e <wild.emmanuelle@gmail.com>
2022-09-07 12:41:09 +0200

    learn: finished nuxt content tutorial

---
+++ content/articles/my-first-blog-post.md
@@ -0,0 +1,38 @@
+---
+title: My first Blog Post
+description: Learning how to use @nuxt/content to create a blog
+img: meow.jpg
+alt: my first blog post
+
+author:
+  name: Benjamin
+  bio: All about Benjamin
+  image: https://images.unsplash.com/.....
+  
+---
+
+<div class="p-4 mb-4 text-white bg-blue-500">
+  This is HTML inside markdown that has a class of note
+</div>
+
+<info-box>
+  <template #info-box>
+    This is a vue component inside markdown using slots
+  </template>
+</info-box>
+
+
+hopla geiss truc muche
+
+## truc
+
+hopla geiss truc muche
+
+### This is a heading
+
+This is some more info
+
+## This is another heading
+
+This is some more info
+
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,70 @@
+<template>
+  <article>
+    <h1>{{ article.title }}</h1>
+    <nav>
+      <ul>
+        <li v-for="link of article.toc" :key="link.id">
+          <NuxtLink :to="`#${link.id}`" :class="{ 'py-2': link.depth === 2, 'ml-2 pb-2': link.depth === 3 }">{{ link.text }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <p>{{ article.description }}</p>
+    <img :src="require(`~/assets/images/${article.img}`)" :alt="article.alt" />
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+
+    <nuxt-content :document="article" />
+    <author-info :author="article.author"></author-info>
+    <!-- <pre>{{ article }}</pre> -->
+    <prev-next :prev="prev" :next="next" />
+  </article>
+
+</template>
+
+
+<script>
+export default {
+    async asyncData({ $content, params }) {
+        const article = await $content('articles', params.slug).fetch()
+
+        const [prev, next] = await $content('articles')
+          .only(['title', 'slug'])
+          .sortBy('createdAt', 'asc')
+          .surround(params.slug)
+          .fetch()
+
+        return {
+          article,
+          prev,
+          next
+        }
+    },
+    methods: {
+        formatDate(date) {
+            const options = { year: 'numeric', month: 'long', day: 'numeric' }
+            return new Date(date).toLocaleDateString('fr', options)
+        }
+ }
+}
+</script>
+
+<style>
+  .nuxt-content h2 {
+    font-weight: bold;
+    font-size: 28px;
+  }
+  .nuxt-content h3 {
+    font-weight: bold;
+    font-size: 22px;
+  }
+  .nuxt-content p {
+    margin-bottom: 20px;
+  }
+  .icon.icon-link {
+  background-image: url('~assets/svg/icon-hashtag.svg');
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-size: 20px 20px;
+}
+
+</style>
